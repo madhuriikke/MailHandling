ea62efaaa2c74260c7e9eb3951c185f4b3d866cc
dell <dell@dell-PC>
2019-06-23 18:06:11 +0530

    ok

---
+++ src/TestConfig.java
@@ -0,0 +1,19 @@
+
+public class TestConfig {
+	
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "madhuriikke2@gmail.com"; //enter your username
+	public static String password = "8605161037"; //enter your password
+	public static String[] to ={"madhuriikke2@gmail.com"};
+	public static String subject = "Test Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath="D:\\abc.jpg";
+	public static String attachmentName="Error.jpeg";
+	
+	
+//link=	https://myaccount.google.com/lesssecureapps->turned on
+	
+
+}
